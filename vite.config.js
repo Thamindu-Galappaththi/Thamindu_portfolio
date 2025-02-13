94e49f9b8b971d286b160108963114b36a00cd5e
Thamindu <thamindusharitha@gmail.com>
2025-02-13 17:26:16 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/MyPortfolio/" //same name as your repo
+  base:"/Thami_Portfolio" //same name as your repo
 })
