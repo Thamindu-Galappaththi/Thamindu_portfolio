419d5b9ad9b043b547b94a196a886933ea78da60
Thamindu <thamindusharitha@gmail.com>
2025-02-14 13:59:26 +0530

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Thamindu_Portfolio" //same name as your repo
+  base:"/Thamindu_portfolio" //same name as your repo
 })
